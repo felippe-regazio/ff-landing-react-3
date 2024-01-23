c00d8443fb68a7c58cc7f8dc9e91af16010dcabf
Felippe Regazio <felippe.moraes@zoho.com>
2024-01-23 04:37:49 -0300

    configured build base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ff-landing-react-3',
+  base: '/ff-landing-react-3/',
 
   build: {
     outDir: './docs'
